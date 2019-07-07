28ad825c9d35e5c69f41ee3547e7e90081f32fe4
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-07-07 14:40:36 +0100

    Alert button

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-function firstLetter (elements, character) {
-    var bNames= "";
-
-    for (var i = 0; i < elements.length; i += 1) {
-        if (elements[1][0] === charset) {
-            bNames = elements[i] + " " + bNames;
-        } else {
-
-        }
-    }
-    return bNames.trim();
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var button = document.getElementById("button");
+    button.onclick = function(){alert("Alert on web!")};
+});
